5ba59d4c0823b5cc82a13e2950e1c5ccffd82f7a
Alex Provost <iphonefaveur@hotmail.com>
2020-01-19 23:11:55 -0500

    Switching walls and moving zone

---
+++ Rock and Smash/Assets/GameMaster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+
+    public GameObject[] borders;
+    public float invokeRate = 1f;
+
+    void Start()
+    {
+        InvokeRepeating("ChooseBorder", 1f, invokeRate);
+
+    }
+
+ 
+    void ChooseBorder()
+    {
+        int indexNumber = Random.Range(0, borders.Length);
+
+        foreach (GameObject border in borders)
+        {
+            if (border.name == borders[indexNumber].name)
+            {
+                border.GetComponent<Image>().color = Color.green;
+            } else
+            {
+                border.GetComponent<Image>().color = Color.red;
+
+            }
+        }
+    }
+}
---
+++ Rock and Smash/Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    GameObject movingSpace;
+
+    void Start()
+    {
+        movingSpace = GameObject.FindGameObjectWithTag("movingSpace");
+
+        //Debug.Log("Height: " + movingSpace.GetComponent<RectTransform>().sizeDelta.y + " Width: " + movingSpace.GetComponent<RectTransform>().sizeDelta.x);
+    }
+
+    void Update()
+    {
+        FollowCursor();
+    }
+
+    void FollowCursor () {
+
+        float movingSpaceHeight = movingSpace.GetComponent<RectTransform>().sizeDelta.y;
+        float movingSpaceWidth = movingSpace.GetComponent<RectTransform>().sizeDelta.x;
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 mouseFollow = Vector2.Lerp(transform.position, mousePosition, 10000000f);
+
+        //float clampX = Mathf.Clamp(mouseFollow.x, movingSpaceWidth / 2 * -1, movingSpaceWidth / 2);
+        //float clampY = Mathf.Clamp(mouseFollow.y, movingSpaceHeight / 2 * -1, movingSpaceHeight / 2);
+
+        float clampX = Mathf.Clamp(mouseFollow.x, -6.8f, 6.8f);
+        float clampY = Mathf.Clamp(mouseFollow.y, -2.9f, 2.9f);
+
+        transform.position = new Vector3(clampX, clampY);
+     
+    }
+}
